564a4e99eb2e47370483f5dc3c432c694997c349
Asia <asia.dobrowolska95@gmail.com>
2020-08-11 17:06:21 +0200

    delete option link in head title

--- src/components/main-components/Header.js
+++ src/components/main-components/Header.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import titleIcon from "../../image/title-icon.png";
 
 const Header = () => {
   return (
     <header className="head">
-      <h1 className="head__title">
-        <NavLink to="/" className="link">
-          personal notebook
-        </NavLink>
-      </h1>
+      <h1 className="head__title">personal notebook</h1>
       <img src={titleIcon} alt="notes" className="head__image" />
     </header>
   );
